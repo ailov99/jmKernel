4f16ca7d12f28654f8cb0024e40aac9103aa5a2c
ailov <rammluft@gmail.com>
2015-11-10 22:48:32 +0000

    removed leftover files

--- run_bochs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# mount correct loopback device -> run bochs on it -> disconnect it
-
-sudo /sbin/losetup /dev/loop0 floppy.img
-sudo bochs -f bochsrc.txt
-sudo /sbin/losetup -d /dev/loop0
