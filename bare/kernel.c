034dc26541cf46447e408af5ac2873e301fa64da
ailov <rammluft@gmail.com>
2015-11-12 21:30:45 +0000

    Fully implemented basic kernel. System now bootable

--- bare/kernel.c
+++ bare/kernel.c
@@ -60,20 +60,69 @@ size_t strlen(const char* str)
 static const size_t VGA_WIDTH = 80;
 static const size_t VGA_HEIGHT = 25;
 
+// track current row/column
 size_t terminal_row;
 size_t terminal_column;
 uint8_t terminal_color;
 uint16_t* terminal_buffer;
 
-
-
-void terminal_initialize()
+void terminal_setcolor(uint8_t color)
 {
+  terminal_color = color;
 }
 
+// Put a vga entry to the buffer by coordinates
+void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
+{
+  const size_t index = y * VGA_WIDTH + x;  // translate the coords to single dimension
+  terminal_buffer[index] = make_vgaentry(c, color);
+}
+
+// write a single char to the terminal
+// use default set color and current coordinates
+void terminal_putchar(char c)
+{
+  terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+
+  // Handle wrap-arounds
+  if (++terminal_column == VGA_WIDTH) {
+	// reset x-coord
+	terminal_column = 0;
+	if (++terminal_row == VGA_HEIGHT)
+	  // reset y-coord
+	  // TODO: implement scrolling
+	  terminal_row = 0;
+  }
+
+}
+
+// Write a whole string
+// (will wrap around properly)
 void terminal_writestring(const char* data)
 {
+  size_t datalen = strlen(data);
+  for (size_t i = 0; i < datalen; i++)
+	terminal_putchar(data[i]);
 }
+
+// Initialize our terminal with light_grey on a black bg
+void terminal_initialize()
+{
+  terminal_row = 0;
+  terminal_column = 0;
+  terminal_color = make_color(COLOR_LIGHT_GREY, COLOR_BLACK);
+  terminal_buffer = (uint16_t*) 0xB8000;
+  
+  for (size_t y = 0; y < VGA_HEIGHT; y++) {
+	for (size_t x = 0; x < VGA_WIDTH; x++) {
+	  // set up buffer with empty entries
+	  // 80-25 map
+	  const size_t index = y * VGA_WIDTH + x;
+	  terminal_buffer[index] = make_vgaentry(' ', terminal_color);
+	}
+  }
+}
+
 
 #if defined(__cplusplus)
 extern "C"  // use C linkage for kernel_main
